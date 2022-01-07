678551694f8065ddaa514e292b3dbe148a487119
sarthakagrawal34 <65223774+sarthakagrawal34@users.noreply.github.com>
2022-01-07 17:35:23 +0530

    Create todoData.js

---
+++ src/todoData.js
@@ -0,0 +1,29 @@
+const todos = [
+  {
+    id: 1,
+    text: "My task 1",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "My task 2",
+    completed: true
+  },
+  {
+    id: 3,
+    text: "My task 3",
+    completed: true
+  },
+  {
+    id: 4,
+    text: "My task 4",
+    completed: true
+  },
+  {
+    id: 5,
+    text: "My task 5",
+    completed: true
+  }
+];
+
+export default todos;
